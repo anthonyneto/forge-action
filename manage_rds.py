90bf40a078f14a99df4ca2c82750ca49140cc22e
Anthony Neto <anthony.neto@gmail.com>
2024-09-09 16:41:13 -0700

    wip

--- manage_rds.py
+++ manage_rds.py
@@ -20,7 +20,6 @@ def create_or_update_security_group(
   description,
   vpc_id
 ):
-  print(group_name)
   try:
     response = ec2_client.describe_security_groups(Filters=[
       {
@@ -100,6 +99,8 @@ def create_rds_instance(
   backup_retention_period=0,
   db_subnet_group_name=None
 ):
+  print(db_instance_id)
+
   security_group_id = create_or_update_security_group(
     group_name=db_instance_id,
     description=f'Security group for RDS: {db_instance_id}',

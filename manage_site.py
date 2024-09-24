e0a0e4bb3eb2e89c25a15de4864b2c31131830bf
Anthony Neto <anthony.neto@gmail.com>
2024-09-23 20:10:23 -0700

    wip

--- manage_site.py
+++ manage_site.py
@@ -23,7 +23,7 @@ def get_sites(api_token, server_id):
   headers = {'Authorization': f'Bearer {api_token}'}
   try:
     response = requests.get(url, headers=headers)
-    print(f'get_sites: {get_sites}')
+    print(f'get_sites: {response}')
     response.raise_for_status()
     return response.json().get('sites', [])
   except requests.RequestException as e:
